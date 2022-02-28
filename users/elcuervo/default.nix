8b3f378724b03a2e664c69f94c6bbcaf9e114017
elcuervo <elcuervo>
2022-02-27 21:21:48 -0300

    Test bash switch

--- users/elcuervo/default.nix
+++ users/elcuervo/default.nix
@@ -63,7 +63,7 @@ in
     users = {
       "${username}" = {
         description = "${fullname}";
-        shell = pkgs.zsh;
+        shell = pkgs.bash;
         home = "/Users/${username}";
       };
     };
@@ -82,6 +82,73 @@ in
       home-manager.enable = true;
 
       bat.enable = true;
+
+      bash = {
+         enable = true;
+
+         initExtra = ''
+           . $HOME/.tokens
+         '';
+
+         shellAliases = {
+           g = "${pkgs.git}/bin/git";
+           gs = "${pkgs.git}/bin/git status";
+           gp = "${pkgs.git}/bin/git pull --rebase";
+
+           cat = "${pkgs.bat}/bin/bat";
+           lg = "${pkgs.lazygit}/bin/lazygit";
+
+           tmn = "${pkgs.tmux}/bin/tmux -S /tmp/pair new-session -s $1";
+           tma = "${pkgs.tmux}/bin/tmux -S /tmp/pair attach-session -t $1";
+           tml = "${pkgs.tmux}/bin/tmux -S /tmp/pair list-sessions";
+
+           md = "mkdir -pv";
+
+           ":q" = "exit";
+         };
+      };
+
+       tmux = {
+         enable = true;
+         baseIndex = 1;
+         keyMode = "vi";
+         historyLimit = 100000;
+         terminal = "screen-256color";
+
+         prefix = "C-a";
+
+         extraConfig = ''
+           bind-key R                refresh-client
+           bind-key x                kill-pane
+           bind-key C-a              last-window
+           bind-key C-s              next-window
+           bind-key C-d              previous-window
+           bind-key |                split-window -h
+           bind-key \\               split-window -h
+           bind-key -                split-window -v
+           bind p paste-buffer
+           set -g set-clipboard on
+           set -g mouse on
+  # UI
+           set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
+  # Current window
+           set-window-option -g window-status-current-format "#[fg=colour237, bg=colour214, nobold, noitalics, nounderscore] #[fg=colour239, bg=colour214]#I #[fg=colour239, bg=colour214, bold]#[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
+  # Other windows
+           set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics] #[fg=colour223,bg=colour239]#I #[fg=colour223, bg=colour239]#[fg=colour239, bg=colour237, noitalics]"
+  # Right status
+           set-option -g status-right "#[fg=colour239, bg=colour237, nobold, nounderscore, noitalics] #[fg=colour246,bg=colour239]#[fg=colour248, bg=colour239, nobold, noitalics, nounderscore] #[fg=colour237, bg=colour248] #h "
+         '';
+
+         plugins = with pkgs.tmuxPlugins; [
+           yank
+           sensible
+           resurrect
+           vim-tmux-navigator
+
+           gruvbox
+         ];
+
+       };
 
       alacritty = import ./programs/alacritty.nix { inherit pkgs; };
       direnv = import ./programs/direnv.nix { inherit pkgs; };
