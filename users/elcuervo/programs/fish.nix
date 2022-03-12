043b31baea4eab9d4c64faa9669905f4ce8231d5
elcuervo <elcuervo@elcuervo.net>
2022-03-12 11:28:41 -0300

    Fish config

--- users/elcuervo/programs/fish.nix
+++ users/elcuervo/programs/fish.nix
@@ -3,6 +3,28 @@
 {
   enable = true;
 
+  plugins = with pkgs; [
+    {
+      name = "z";
+      src = fetchFromGitHub {
+        owner = "jethrokuan";
+        repo = "z";
+        rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+        sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+      };
+    }
+  ];
+
+  shellInit = ''
+    set fish_greeting ""
+
+    # theme
+    set -g theme_color_scheme terminal-dark
+    set -g fish_prompt_pwd_dir_length 1
+    set -g theme_display_user no
+    set -g theme_hide_hostname yes
+  '';
+
   shellAliases = {
     g = "${pkgs.git}/bin/git";
     gs = "${pkgs.git}/bin/git status";
