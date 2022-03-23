428ded008639bb95b15b20d29e64e7078954fbd1
elcuervo <elcuervo@elcuervo.net>
2022-03-23 18:49:59 -0300

    Upgrade dockutil

--- overlays/dockutil/default.nix
+++ overlays/dockutil/default.nix
@@ -3,11 +3,11 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "dockutil";
-  version = "2.0.5";
+  version = "3.0.2";
 
   src = fetchurl {
     url = "https://github.com/kcrawford/dockutil/releases/download/${version}/dockutil-${version}.pkg";
-    sha256 = "sha256-kZ7dOG8SSh25DgcbvkgPvEFGZIVz4fv0kWN41pbeNlw=";
+    sha256 = "sha256-F1E36nR+g+0iHWCxi3ErJW7TFTFTTN6E9nlIfTN2aP0=";
   };
 
   dontBuild = true;
