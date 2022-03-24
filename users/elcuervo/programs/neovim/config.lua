5d85684700fc4029d36093a53937ff4982965c30
elcuervo <elcuervo@elcuervo.net>
2022-03-24 13:22:43 -0300

    theme config

--- users/elcuervo/programs/neovim/config.lua
+++ users/elcuervo/programs/neovim/config.lua
@@ -1,7 +1,21 @@
 require('nvim-web-devicons').setup()
 
 require('onedark').setup {
-    style = 'warmer'
+  style = 'warmer',
+  code_style = {
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'none',
+    strings = 'none',
+    variables = 'none'
+  },
+
+  -- Plugins Config --
+  diagnostics = {
+    darker = true, -- darker colors for diagnostic
+    undercurl = true,   -- use undercurl instead of underline for diagnostics
+    background = true,    -- use background color for virtual text
+  },
 }
 
 require('onedark').load()
