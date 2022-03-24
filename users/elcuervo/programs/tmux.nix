4fe0e9d93f675237f1bdf5ace5aa4e17ebea02f0
elcuervo <elcuervo@elcuervo.net>
2022-03-24 11:45:38 -0300

    Configure nova

--- users/elcuervo/programs/tmux.nix
+++ users/elcuervo/programs/tmux.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 
+let
+  tmux-nova = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "nova";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "o0th";
+      repo = "tmux-nova";
+      rev = "3e56f80a12b284cd33ab25d58671799e983dc70c";
+      sha256 = "sha256-xT4V6ot1o9A8hNKr3mAr7pgl9FYlFxcionR3vIYAfzY=";
+    };
+  };
+in
 {
   enable = true;
   baseIndex = 1;
@@ -7,37 +19,47 @@
   historyLimit = 100000;
   terminal = "screen-256color";
 
+  sensibleOnTop = true;
+
   prefix = "C-a";
 
   extraConfig = ''
-             bind-key R                refresh-client
-             bind-key x                kill-pane
-             bind-key C-a              last-window
-             bind-key C-s              next-window
-             bind-key C-d              previous-window
-             bind-key |                split-window -h
-             bind-key \\               split-window -h
-             bind-key -                split-window -v
-             bind p paste-buffer
-             set -g set-clipboard on
-             set -g mouse on
-    # UI
-             set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
-    # Current window
-             set-window-option -g window-status-current-format "#[fg=colour237, bg=colour214, nobold, noitalics, nounderscore] #[fg=colour239, bg=colour214]#I #[fg=colour239, bg=colour214, bold]#[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
-    # Other windows
-             set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics] #[fg=colour223,bg=colour239]#I #[fg=colour223, bg=colour239]#[fg=colour239, bg=colour237, noitalics]"
-    # Right status
-             set-option -g status-right "#[fg=colour239, bg=colour237, nobold, nounderscore, noitalics] #[fg=colour246,bg=colour239]#[fg=colour248, bg=colour239, nobold, noitalics, nounderscore] #[fg=colour237, bg=colour248] #h "
+    bind-key R refresh-client
+    bind-key x kill-pane
+    bind-key C-a last-window
+    bind-key C-s next-window
+    bind-key C-d previous-window
+    bind-key | split-window -h
+    bind-key \\ split-window -h
+    bind-key - split-window -v
+    bind p paste-buffer
+
+    set -g set-clipboard on
+    set -g mouse on
+
+    set -g @plugin 'o0th/tmux-nova'
+
+    set -g @nova-nerdfonts false
+    set -g @nova-nerdfonts-left ""
+    set -g @nova-nerdfonts-right ""
+
+    set -g @nova-segment-mode "#S"
+    set -g @nova-segment-mode-colors "#50fa7b #282a36"
+
+    set -g @nova-segment-whoami "#h"
+    set -g @nova-segment-whoami-colors "#50fa7b #282a36"
+
+    set -g @nova-pane "#I#{?pane_in_mode,  #{pane_mode},}"
+
+    set -g @nova-rows 0
+    set -g @nova-segments-0-left "mode"
+    set -g @nova-segments-0-right "whoami "
   '';
 
   plugins = with pkgs.tmuxPlugins; [
     yank
-    sensible
-    resurrect
     vim-tmux-navigator
 
-    gruvbox
+    tmux-nova
   ];
-
 }
