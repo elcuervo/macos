7609433c152f49d527b31109446d5d585fe8614e
elcuervo <elcuervo@elcuervo.net>
2023-02-18 17:05:59 -0300

    Simplified version

--- users/elcuervo/programs/neovim.nix
+++ users/elcuervo/programs/neovim.nix
@@ -14,26 +14,29 @@ in
 
 {
   enable = true;
-  vimAlias = true;
-  vimdiffAlias = true;
   package = pkgs.neovim-unwrapped;
 
   withNodeJs = false;
   withPython3 = false;
   withRuby = false;
 
-  extraConfig = ''
-    syntax on
-  '';
-
   extraPackages = with pkgs; [
     yaml-language-server
+    dhall-lsp-server
   ];
 
   plugins = with pkgs.vimPlugins; [
+    # General
     supertab
     sensible
     nerdtree
+    mkdir-nvim
+
+    # Navigation
+    plenary-nvim
+    telescope-nvim
+    telescope-fzf-native-nvim
+    telescope-ui-select-nvim
 
     # UI
     {
@@ -44,5 +47,24 @@ in
     lualine-nvim
     nvim-web-devicons
     trouble-nvim
+
+    vim-bbye
+    gitsigns-nvim
+
+    vim-fugitive
+    webapi-vim
+    vim-gist
+    vim-tmux-navigator
+    vim-carbon-now-sh
+
+    # LSP
+    nvim-lspconfig
+
+    # Syntax
+    nvim-treesitter.withAllGrammars
+    nvim-treesitter-refactor
+    nvim-treesitter-endwise
+    vim-matchup
+    dhall-vim
   ];
 }
